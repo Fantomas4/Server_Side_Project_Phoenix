abd9af59626fbeb485743296ca27d027e409aff9
kongr45gpen <electrovesta@gmail.com>
2017-01-02 22:42:39 +0200

    Add vote tests

--- Phoenix_test.py
+++ Phoenix_test.py
@@ -6,11 +6,27 @@ class PhoenixTestCase(unittest.TestCase):
         phoenix.app.config['TESTING'] = True
         self.app = phoenix.app.test_client()
 
+    def __assert_ok(self, url):
+        rv = self.app.get(url)
+        assert b'OK' in rv.data
+
     def test_song_count(self):
         rv = self.app.get('/song_count/set/44')
         assert b'OK' in rv.data
         rv = self.app.get('/song_count/get')
         assert b'44' in rv.data
+
+    def test_vote_result(self):
+        self.__assert_ok('/song_count/set/50')
+        for i in range(0,10):
+            self.__assert_ok('/vote/record/25')
+        for i in range(0,25):
+            self.__assert_ok('/vote/record/14')
+        for i in range(0,24):
+            self.__assert_ok('/vote/record/47')
+
+        rv = self.app.get('/vote/result')
+        assert b'14' in rv.data
 
 
 if __name__ == '__main__':
