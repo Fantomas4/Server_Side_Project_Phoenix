0d6f1ee264a8939682fdb0201cfc294fdb0f78a2
Fantomas4 <skaramperas@cs.uth.gr>
2017-01-02 21:47:53 +0200

    added votes

--- Project_Phoenix.py
+++ Project_Phoenix.py
@@ -1,7 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-
+votes={}
 song_count=0
+
 
 @app.route('/song_count/get')
 def get_song_count():
@@ -15,6 +16,20 @@ def set_song_count(count):
     song_count=count
     return "OK"
 
+@app.route('/vote/record/<int:choice>')
+def vote_record(choice):
+    global votes
+    if not choice in votes: votes[choice] = 0
+    votes[choice]+=1
+    return "OK"
+
+@app.route('/vote/debug')
+def vote_debug():
+    global votes
+    answer = "<pre>"
+    for i in votes:
+        answer = answer + "%d -> %d<br>" % (i, votes[i])
+    return answer
 
 
 
